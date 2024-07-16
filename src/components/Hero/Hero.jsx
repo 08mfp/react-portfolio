dd107ca427ad5fa07f07842c6eef4bfb65ea4992
08mfp <mohamedfaridpatel@gmail.com>
2024-07-16 09:32:42 +0100

    updated contact button in about section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,19 +9,19 @@ export const Hero = () => {
         <h1 className={styles.title}>Hi, I'm Mohamed Farid Patel</h1>
         <p className={styles.description}>
           I am a Third-year Computer Science student at The University of Manchester, graduating in July 2025.
-          I have worked on multiple projects, ranging from web development to machine learning, which you can read more about below.
+          I have worked on multiple projects, ranging from web development to machine learning, which you can check out on this website or on my LinkedIn.
           <br />
           <br />
           {/* If you have any questions, or would like to reach out, you can find my contact details at the bottom of this page. */}
-          If you have any questions, or would like to reach out, you can use the button below.
+          {/* If you have any questions, or would like to reach out, you can find my details below: */}
         </p>
         <div style={{ display: 'flex' }}>
-          <a href="mailto:contact@mohfarid.com" className={styles.contactBtn} style={{ marginRight: '10px' }}>
+          {/* <a href="mailto:contact@mohfarid.com" className={styles.contactBtn} style={{ marginRight: '10px' }}>
             Email Me
-          </a>
-          {/* <a href="https://www.linkedin.com/in/mohamedfaridpatel" className={styles.contactBtn}>
-            My LinkedIn
           </a> */}
+          <a href="/#contact" className={styles.contactBtn}>
+            Contact Me
+          </a>
         </div>
       </div>
       <img
